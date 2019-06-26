15f62e00adb448a7e81b08eab0ce9d6b09151629
miwashuntaro0611 <miwashuntaro19245@gmail.com>
2019-06-26 16:09:37 +0900

    位置修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,7 @@ const typescript =  require('./loaders/typescript')
 const vue =  require('./loaders/vue')
 const pug = require('./loaders/pug')
 
+environment.loaders.append('typescript', typescript)
 environment.loaders.append('vue', vue)
 environment.loaders.prepend('pug', pug)
-environment.loaders.append('typescript', typescript)
 module.exports = environment
